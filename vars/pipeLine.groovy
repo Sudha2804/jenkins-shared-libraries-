f395cab48391da640b7c4101ad5070303ec9fcbc
Sudha G C <158569395+Sudha2804@users.noreply.github.com>
2025-01-05 07:45:04 +0530

    Update pipeLine.groovy

--- vars/pipeLine.groovy
+++ vars/pipeLine.groovy
@@ -1,66 +1,29 @@
-def build_Project() {
-    echo 'Building project with Maven...'
-    sh 'mvn clean package'
-}
-
-def checkout_code() {
+def checkscm() {
     echo 'Checking out code...'
     checkout scm
 }
-
-def clean_up() {
-    echo 'Cleaning up...'
-    sh 'pkill -f "mvn spring-boot:run" || true'
+def setupjava() {
+    echo 'Setting up Java 17...'
+    sh 'sudo apt update'
+    sh 'sudo apt install -y openjdk-17-jdk'
 }
-
-def cleanup_Workspace() {
-    echo 'Cleaning up the workspace...'
-    deleteDir() // Deletes all files in the current workspace
+def mavensetup() {
+    echo 'Setting up Maven...'
+    sh 'sudo apt install -y maven'
 }
-
-def run_Application() {
+def build() {
+    echo 'Building project with Maven...'
+    sh 'mvn clean package'
+}
+def runApp() {
     echo 'Running Spring Boot application...'
     sh 'nohup mvn spring-boot:run &'
-    
-    // Wait for the application to be up - can be replaced with a better check
     sleep(time: 15, unit: 'SECONDS')
 
     def publicIp = sh(script: "curl -s https://checkip.amazonaws.com", returnStdout: true).trim()
     echo "The application is running and accessible at: http://${publicIp}:8080"
 }
-
-def setup_Java() {
-    echo 'Setting up Java 17...'
-    sh 'sudo apt update'
-    sh 'sudo apt install -y openjdk-17-jdk'
-}
-
-def setup_Maven() {
-    echo 'Setting up Maven...'
-    sh 'sudo apt install -y maven'
-}
-
-def stop_Application() {
-    echo 'Gracefully stopping the Spring Boot application...'
-    sh 'mvn spring-boot:stop'
-}
-
-def tag_Build(String tagName, String message = 'Build tagging') {
-    echo "Tagging the build with tag: ${tagName}"
-    
-    // Tagging the current commit in Git
-    sh """
-        git tag -a ${tagName} -m '${message}'
-        git push origin ${tagName}
-    """
-}
-
-def upload_Artifact(String artifactPath) {
-    echo 'Uploading artifact...'
-    archiveArtifacts artifacts: artifactPath, allowEmptyArchive: true
-}
-
-def validate_App() {
+def validateApp() {
     echo 'Validating that the app is running...'
     def response = sh(script: 'curl --write-out "%{http_code}" --silent --output /dev/null http://localhost:8080', returnStdout: true).trim()
     if (response == "200") {
@@ -69,4 +32,16 @@ def validate_App() {
         echo "The app failed to start. HTTP response code: ${response}"
         error("The app did not start correctly!")
     }
+}
+def waiting() {
+        echo 'Waiting for 2 minutes...'
+        sleep(time: 2, unit: 'MINUTES')  // Wait for 2 minutes
+}
+def stop() {
+    echo 'Gracefully stopping the Spring Boot application...'
+    sh 'mvn spring-boot:stop'
+}
+def clean() {
+    echo 'Cleaning up...'
+    sh 'pkill -f "mvn spring-boot:run" || true'
 }
